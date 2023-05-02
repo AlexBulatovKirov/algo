0664bf04bfba2b85ac8247470e41c3776d95eee7
igryhead <131183122+igryhead@users.noreply.github.com>
2023-05-02 19:22:01 +0300

    это работает(наверно)

--- alexandro.py
+++ alexandro.py
@@ -1,11 +1,9 @@
 from pygame import *
-from random import *
-img_hero = 'rocket.png'
+img_hero = 'racket.png'
 img_back = 'galaxy.jpg'
-img_ball = 'ufo.png'
+img_ball = 'tenis_ball.png'
 
 class GameSprite(sprite.Sprite):
-
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         sprite.Sprite.__init__(self)
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
@@ -20,7 +18,7 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update(self):
         keys = key.get_pressed()
-        if keys[K_DOWN] and self.rect.y < 700:
+        if keys[K_DOWN] and self.rect.y < 660:
             self.rect.y += self.speed
         if keys[K_UP] and self.rect.y > 10 :
             self.rect.y -= self.speed
@@ -28,76 +26,85 @@ class Player(GameSprite):
 class Player1(GameSprite):
     def update(self):
         keys = key.get_pressed()
-        if keys[K_s] and self.rect.y <700:
+        if keys[K_s] and self.rect.y < 660:
             self.rect.y += self.speed
         if keys[K_w] and self.rect.y > 10 :
             self.rect.y -= self.speed    
 
-font.init()
-font1 = font.SysFont('Arial', 36)
-lost = False
 
-class Ball(GameSprite):
-    lost = False
-    def update(self):
-        self.rect.x += self.speed
-        self.rect.y += self.speed
-        if self.rect.x > 800:
-            self.rect.x -= self.speed
-        if self.rect.x < 0:
-            self.speed = 5
-        if self.rect.y < 600:
-            self.speed = -5
-        if self.rect.y > 0:
-            self.speed = 5
-
-        
-
-
+  
 win_width = 1000
 win_height = 800
 display.set_caption("Пинпонг")
 window = display.set_mode((win_width, win_height))
 background = transform.scale(image.load(img_back), (win_width, win_height))
 
-ship = Player(img_hero, 20, win_height - 100, 80, 100, 10)
-ship1 = Player1(img_hero, 900, 400, 80, 100, 10)
-ball = Ball(img_ball, 400, 100, 50, 50, 10 )
+ship = Player(img_hero, 20, 380, 50, 140, 10)
+ship1 = Player1(img_hero, 930, 350, 50, 140, 10)
+ball = GameSprite(img_ball, 400, 100, 50, 50, 5)
 
 finish = False
 run = True
+clock = time.Clock()
+FPS = 60
 
+check = 0
+check1 = 0
+speed_x = 5
+speed_y = 5
+
+font.init()
+font = font.Font(None, 70)
+win1 = font.render('Пебедил первый игрок', True, (255, 0, 0))
+win2 = font.render('Пебедил второй игрок', True, (255, 0, 0))
+
+count1 = font.render('Счёт:' + str(check), True, (0, 255, 0))
+count2 = font.render('Счёт:' + str(check1), True, (0, 255, 0))
 
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
         
-        
-
     if not finish:
         window.blit(background,(0,0))
-
 
         ship.update()
         ship.reset()
         ship1.update()
         ship1.reset()
-        ball.update()
         ball.reset()
-        if lost == True:
-            run = False
-
-        #collides = sprite.groupcollide(monsters, bulls, True, True)
-        #if collides:
-        #    moster = Enemy('ufo.png', randint(80, 620), -50, 80, 50, randint(1, 3))
-        #    monsters.add(moster)
-        #    score+=1
-        #    if top <= score:
-        #        top+=1
+        window.blit(count1, (10, 20))
+        window.blit(count2, (830, 20))
         
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
 
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= -1
+        if ball.rect.x > win_width - 50 or ball.rect.x < 0:
+            speed_x *= -1
+
+        if sprite.collide_rect(ship, ball) or sprite.collide_rect(ship1, ball):
+            speed_x *= -1
+
+        if sprite.collide_rect(ship, ball):
+            check += 1
+            count1 = font.render('Счёт:' + str(check), True, (0, 255, 0))
+        
+        if sprite.collide_rect(ship1, ball):
+            check1 += 1
+            count2 = font.render('Счёт:' + str(check1), True, (0, 255, 0))
+
+        if ball.rect.x > ship1.rect.x:
+            finish = True
+            window.blit(win1, (250, 300))
+
+        if ball.rect.x < ship.rect.x:
+            finish = True
+            window.blit(win2, (250, 300))
 
     display.update()
 
     time.delay(15)
+
