d1c2d07fd90a2aee8102f99a7b4793b539f2ab0c
AlexBulatov <bulatov@vtlkirov.ru>
2023-05-02 17:38:26 +0300

    ОСНОВА ПРОГРАММЫ + ДВИЖЕНИЕ ВЛЕВО_ВПРАВО

--- alex.py
+++ alex.py
@@ -1,16 +1,11 @@
 from pygame import *
 
 
-#нам нужны такие картинки:
-img_back = "galaxy.jpg" #фон игры
-img_hero = "rocket.png" #герой
-
-#класс-родитель для других спрайтов
+# класс-родитель для спрайтов
 class GameSprite(sprite.Sprite):
-
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-        sprite.Sprite.__init__(self)
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight, height))  # вместе 55,55 - параметры
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -19,36 +14,57 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+
 class Player(GameSprite):
-    def update(self):
+    def update_r(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
-    def fire(self):
-        pass
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
-win_width = 700
+
+# игровая сцена:
+back = (200, 255, 255)  # цвет фона (background)
+win_width = 600
 win_height = 500
-display.set_caption("Shooter")
 window = display.set_mode((win_width, win_height))
-background = transform.scale(image.load(img_back), (win_width, win_height))
+window.fill(back)
 
-ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
-
+# флаги, отвечающие за состояние игры
+game = True
 finish = False
-run = True
+clock = time.Clock()
+FPS = 60
 
-while run:
+# создания мяча и ракетки
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+
+
+while game:
     for e in event.get():
         if e.type == QUIT:
-            run = False
-    if not finish:
-        window.blit(background,(0,0))
-        ship.update()
-        ship.reset()
-        display.update()
+            game = False
 
-    time.delay(50)
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)
